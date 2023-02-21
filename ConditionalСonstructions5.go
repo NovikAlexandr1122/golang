829d9dc668663e600bd9bde42d28c396524bd214
Alexandr <novik.a.95@yandex.by>
2023-02-21 13:02:24 +0300

    Completing tasks.

--- ConditionalСonstructions5.go
+++ ConditionalСonstructions5.go
@@ -25,7 +25,7 @@ import "fmt"
 func main() {
 	var a int
 	fmt.Scan(&a)
-	if a%4 == 0 && a%100 != 0 || a%400 == 0 {
+	if a%4 == 0 && a%100 != 0 && a%400 == 0 {
 		fmt.Println("YES.")
 	} else {
 		fmt.Println("NO.")
