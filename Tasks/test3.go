f777bb157442c3e541f5358c5941b08780133df6
Alexandr <novik.a.95@yandex.by>
2023-02-27 10:49:11 +0300

    task3.go

--- Tasks/test3.go
+++ Tasks/test3.go
@@ -4,10 +4,11 @@ import "fmt"
 
 func main() {
 	var a int
-	//var b int
 	fmt.Scan(&a)
-	h := a / 3600
-	m := a % 3600
-	fmt.Println(h, m)
+	if a < 86400 && a >= 0 {
+		h := a / 3600
+		m := (a - (h * 3600)) / 60
 
+		fmt.Printf("It is %d hours %d minutes.", h, m)
+	}
 }
