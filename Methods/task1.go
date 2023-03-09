26e87ffc89c6244c8845fd2707abf4715909a2f3
Alexandr <novik.a.95@yandex.by>
2023-03-09 17:38:10 +0300

    filed test

---
+++ Methods/task1.go
@@ -0,0 +1,37 @@
+package main
+
+type testStruct struct {
+	On    bool
+	Ammo  int
+	Power int
+}
+
+func (s testStruct) Shot() bool {
+
+	isTrue := false
+
+	if s.Ammo > 0 {
+		s.Ammo = s.Ammo - 1
+		isTrue = true
+	} else if s.Ammo < 0 {
+		isTrue = false
+	}
+	return isTrue
+}
+
+func (r testStruct) RideBike() bool {
+
+	isTrue := false
+
+	if r.Power > 0 {
+		r.Power = r.Power - 1
+		isTrue = true
+	} else if r.Power < 0 {
+		isTrue = false
+	}
+	return isTrue
+}
+
+func main() {
+	testStruct := new(testStruct)
+}
